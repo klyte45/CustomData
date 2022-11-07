d0790ed25d1e56265612374c6c031b04c7036b0e
Leandro Klyte <klyte45@hotmail.com>
2022-11-06 21:29:36 -0300

    v1.0.0r3 Updated to latest KU

--- CustomData/ModInstance.cs
+++ CustomData/ModInstance.cs
@@ -6,7 +6,7 @@ using System.Globalization;
 using System.Reflection;
 using UnityEngine;
 
-[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
 namespace CustomData
 {
     public class ModInstance : BasicIUserMod<ModInstance, CDController>
